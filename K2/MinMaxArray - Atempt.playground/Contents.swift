53f0304e0c1c55261fd6bf6c4971095bc764732e
LuthforKhan <lrkhan@umich.edu>
2022-03-14 09:32:07 -0400

    started challanges

---
+++ K2/MinMaxArray - Atempt.playground/Contents.swift
@@ -0,0 +1,80 @@
+import UIKit
+import XCTest
+
+/// finds the minimum and maximum values of an array. If the array is empty, return an empty array. Try to do this without using builtin functions.
+///
+/// - Parameters:
+///     - inputArray: an array of integers
+/// - Returns: Minimum value as 0th element and maximum value as 1st element. An empty input array returns empty output array.
+func findMinMax(inputArray: [Int]) -> [Int] {
+    if inputArray.isEmpty { return [] }
+    
+    var min = inputArray[0]
+    var max = inputArray[0]
+    
+    for i in inputArray {
+        if i <  min{
+            min = i
+        }
+        if i > max {
+            max = i
+        }
+    }
+    
+    return [min, max]
+}
+
+class TestFindMinMax: XCTestCase {
+    
+    func testBasic() {
+        let inputArray = [1, 2, 3, 4, 5]
+        let expectedResult = [1, 5]
+        let actualResult = findMinMax(inputArray: inputArray)
+        XCTAssert(expectedResult == actualResult)
+    }
+    
+    func testRepeatedMin() {
+        let inputArray = [1, 1, 1, 3]
+        let expectedResult = [1, 3]
+        let actualResult = findMinMax(inputArray: inputArray)
+        XCTAssert(expectedResult == actualResult)
+    }
+    
+    func testRepeatedMinReversed() {
+        let inputArray = [3, 1, 1, 1]
+        let expectedResult = [1, 3]
+        let actualResult = findMinMax(inputArray: inputArray)
+        XCTAssert(expectedResult == actualResult)
+    }
+    
+    func testRepeatedMax() {
+        let inputArray = [1, 5, 5, 5]
+        let expectedResult = [1, 5]
+        let actualResult = findMinMax(inputArray: inputArray)
+        XCTAssert(expectedResult == actualResult)
+    }
+    
+    func testHomogenousArray() {
+        let inputArray = [5, 5, 5, 5]
+        let expectedResult = [5, 5]
+        let actualResult = findMinMax(inputArray: inputArray)
+        XCTAssert(expectedResult == actualResult)
+    }
+    
+    func testNegativeValues() {
+        let inputArray = [-5, -1, 0]
+        let expectedResult = [-5, 0]
+        let actualResult = findMinMax(inputArray: inputArray)
+        XCTAssert(expectedResult == actualResult)
+    }
+    
+    func testEmptyArray() {
+        let inputArray = [Int]()
+        let expectedResult = [Int]()
+        let actualResult = findMinMax(inputArray: inputArray)
+        XCTAssert(expectedResult == actualResult)
+    }
+
+}
+
+TestFindMinMax.defaultTestSuite.run()
