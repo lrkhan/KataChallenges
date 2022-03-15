faf216eb2a2aeb6fb471f891b573f9ac11063543
LuthforKhan <lrkhan@umich.edu>
2022-03-15 08:23:20 -0400

    k4 - redefined the pattern a bit

--- K4/Kata4-ValidEmail - Atempt.playground/Contents.swift
+++ K4/Kata4-ValidEmail - Atempt.playground/Contents.swift
@@ -14,7 +14,7 @@ import XCTest
 /// - Returns:
 ///     True if valid, false if invalid
 func checkIfValidEmail(input: String) -> Bool {
-    let pattern = #"(?!.*[._@-]{2,})^[^._-][a-zA-Z0-9._-]+[^._-]@[^._-][a-zA-Z0-9._-]{1,}[^._@-]+\.{1}[a-zA-z]{2,}+$"#
+    let pattern = #"(?!.*[._@-]{2,})^[^._-][a-zA-Z0-9._-]+[^._-]@[^._-][a-zA-Z0-9._-]+[^._@-]+\.[a-zA-z]{2,}+$"#
     let range = NSRange(location: 0, length: input.count)
     let regex = try! NSRegularExpression(pattern: pattern)
 
